584e85bce6d4aad0d69fcd09bd58bc4f43f55d96
vivek <vivek.bnb@gmail.com>
2018-10-18 15:14:06 +0530

    Spectogram Added

--- src/sound_recorder.py
+++ src/sound_recorder.py
@@ -1,15 +1,21 @@
 import pyaudio
 import wave
 import  struct
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 CHUNK = 1024
-RECORD_SECONDS = 10
+RECORD_SECONDS =5
 WAVE_OUTPUT_FILENAME = "file.wav"
+THRESHOLD = 40
 
 audio = pyaudio.PyAudio()
+plot_counter = 0
 
 # start Recording
 stream = audio.open(format=FORMAT, channels=CHANNELS,
@@ -20,8 +26,10 @@ frames = []
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    data_int = struct.unpack(str(4 * CHUNK) + 'B', data)
+    #print(data)
+    data_int = np.array(struct.unpack(str(4 * CHUNK) + 'B', data))
     print(data_int)
+    #samples = array(data_int)
     frames.append(data)
 print("finished recording")
 
@@ -29,6 +37,15 @@ print("finished recording")
 stream.stop_stream()
 stream.close()
 audio.terminate()
+
+amplitude = np.sqrt(np.mean(np.square(data_int)))
+#spectrogram of data
+
+f, t, Sxx = signal.spectrogram(data_int, RATE)
+plt.pcolormesh(t, f, Sxx)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.show()
 
 #write audio into the file
 
