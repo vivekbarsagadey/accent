076c3d9e8093575983f958b6e230c96552af91a5
vivek.barsagadey <vivek.bnb@gmail.com>
2018-12-03 14:23:06 +0530

    added new project for services

---
+++ accent-service-api/src/main/java/com/whizit/accent/AccentServiceApiApplication.java
@@ -0,0 +1,12 @@
+package com.whizit.accent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentServiceApiApplication.class, args);
+	}
+}
