e8742443424bf8537619b66c590f5cba6d5b143c
vivek <vivek.bnb@gmail.com>
2019-02-14 18:37:54 +0530

    block of graph

--- accent-poc/src/distanceMatrix.py
+++ accent-poc/src/distanceMatrix.py
@@ -3,6 +3,7 @@ from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 import librosa
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 x = np.array([[1,1], [2,2], [3,3], [4,4], [5,5]])
 z = np.array([[2,2], [3,3], [4,4]])
@@ -10,17 +11,39 @@ distance, path = fastdtw(x, z, dist=euclidean)
 #print(distance)
 #print(path)
 
-FilePath = 'C:/project/accent/accent-poc/src/Audio/'
+FilePath = 'C:/project/accent/accent-poc/src/Audio/compare/'
 mfccPath = 'C:/project/accent/accent-poc/src/csvFiles/'
+dirs = [f for f in os.listdir(FilePath)]
+recordings = []
+for audio in dirs:
+    if audio.endswith('.wav'):
+        recordings.append(audio)
+print(recordings)
+# y, sr = librosa.load(FilePath+'speaker2.wav')
+# temp = librosa.feature.mfcc(y=y, sr=sr ,n_mfcc=12)
+# mfcc = temp.T        #Here it did transpose because mfcc is 2 dimensional
+# print('mfcc array',mfcc)
+# df = pd.DataFrame(mfcc)
+# df.to_csv(mfccPath + 'mfcc_amol.csv')
+# print('mfcc diamension',mfcc.shape)
+# plt.figure(figsize=(10, 4))
+# plt.plot(mfcc)
+# plt.title('MFCC')
+# plt.show()
 
-y, sr = librosa.load(FilePath+'speaker2.wav')
-temp = librosa.feature.mfcc(y=y, sr=sr ,n_mfcc=12)
-mfcc = temp.T        #Here it did transpose because mfcc is 2 dimensional
-print('mfcc array',mfcc)
-df = pd.DataFrame(mfcc)
-df.to_csv(mfccPath + 'mfcc_amol.csv')
-print('mfcc diamension',mfcc.shape)
-plt.figure(figsize=(10, 4))
-plt.plot(mfcc)
-plt.title('MFCC')
+
+fig = plt.figure(figsize=(12, 6))
+plt.subplots_adjust(hspace=0.5)
+for index, filename in enumerate(recordings, start=1):
+    y, sr = librosa.load(str(FilePath) + filename)
+    temp = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
+    mfcc = temp.T  # Here it did transpose because mfcc is 2 dimensional
+    print('mfcc array', mfcc)
+    df = pd.DataFrame(mfcc)
+    df.to_csv(mfccPath + filename +'.csv')
+    print('mfcc diamension', mfcc.shape)
+    plt.subplot(2,1, index)
+    plt.title('Frequency of BIRD word of ' + filename)
+    plt.plot(mfcc)
+    plt.grid()
 plt.show()
